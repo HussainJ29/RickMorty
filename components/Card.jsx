8a16e858fdf212fdfd0916750d9589cb87c2182a
hussainsidhpurwala <hussain.612059.it@mhssce.ac.in>
2024-08-12 12:55:29 +0530

    first commit

---
+++ components/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+        <div className='card'>
+          <div className='img01'>
+            <img src="{image}" alt="image" />
+          </div>
+          <div className="content">
+            <div className='sec-1'>
+              <a href="#"><h2>Name</h2></a>
+              <span><a href="#">Alive-human</a></span>
+            </div>
+            <div className='sec-2'>
+              <span>Last Known Location</span><br />
+              <a href="#">Earth</a>
+            </div>
+            <div className='sec-3'>
+              <span>First seen in:</span><br />
+              <a href="#">Rixty Minutes</a>
+            </div>
+
+          </div>
+        </div>
+  )
+}
+export default Card
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import '../src/assets/image.PNG';
+import Card from '../components/Card'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="body">
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    </div>
+    </>
+  )
+}
+
+export default App
